fc6e33c49687cb158d8fc8b1798947597f6c6fe4
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-08-21 01:39:55 +0530

    Initial Commit

---
+++ Fragment-Segments-gu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fragment-Segments-gu
+//
+//  Created by Jaimin Raval on 21/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
